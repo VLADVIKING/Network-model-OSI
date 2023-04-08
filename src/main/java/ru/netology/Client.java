130165e6d1ecb81939fbaf88b0507fdb53b4135b
Vlad Khoteenkov <vla_kho@mail.ru>
2023-04-08 14:00:12 +0300

    Run config updates

--- src/main/java/ru/netology/Client.java
+++ src/main/java/ru/netology/Client.java
@@ -5,13 +5,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+
 import java.net.Socket;
 import java.util.Scanner;
 
 import static ru.netology.ServerConfig.HOST;
 import static ru.netology.ServerConfig.PORT;
 
-public class ClientTwo {
+public class Client {
 
     static String input = null;
 
@@ -24,16 +25,15 @@ public class ClientTwo {
             String serverQuery = reader.readLine();
             if (serverQuery.equals("???")) {
                 System.out.println("Введи любое название города:");
-                writer.println(ClientOne.scannerImpl());
+                writer.println(Client.scannerImpl());
             } else {
                 System.out.println("Введи название города, начинающееся на букву: " + serverQuery.charAt(serverQuery.length() - 1));
-                writer.println(ClientOne.scannerImpl());
+                writer.println(Client.scannerImpl());
             }
             System.out.println(reader.readLine());
             writer.flush();
             writer.close();
             reader.close();
-
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,7 +42,7 @@ public class ClientTwo {
 
     public static String scannerImpl() {
         Scanner scanner = new Scanner(System.in);
-        input = scanner.nextLine();
+        input = scanner.nextLine().toLowerCase();
         return input;
     }
 }
